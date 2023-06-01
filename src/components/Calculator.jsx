f53750449519696f59e12e4ffc6dcd66bf76b2ca
Jrmcuervo <jrmcuervom@gmail.com>
2023-06-01 15:03:52 -0600

    Add logic to make the calculator functional

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,50 +1,72 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Calculator.css';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <div className="screen">0</div>
-    <div className="buttons">
-      <div className="row">
-        <Button label="AC" color="light-gray" />
-        <Button label="+/-" color="light-gray" />
-        <Button label="%" color="light-gray" />
-        <Button label="÷" color="orange" />
-      </div>
-      <div className="row">
-        <Button label="7" color="gray" />
-        <Button label="8" color="gray" />
-        <Button label="9" color="gray" />
-        <Button label="×" color="orange" />
-      </div>
-      <div className="row">
-        <Button label="4" color="gray" />
-        <Button label="5" color="gray" />
-        <Button label="6" color="gray" />
-        <Button label="-" color="orange" />
-      </div>
-      <div className="row">
-        <Button label="1" color="gray" />
-        <Button label="2" color="gray" />
-        <Button label="3" color="gray" />
-        <Button label="+" color="orange" />
-      </div>
-      <div className="row">
-        <Button label="0" color="gray" doubleSize />
-        <Button label="." color="gray" />
-        <Button label="=" color="orange" />
+const Calculator = () => {
+  const [displayValue, setDisplayValue] = useState('0');
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    const newData = calculate(calculatorData, buttonName);
+    setCalculatorData(newData);
+
+    const { total, next } = newData;
+    const valueToDisplay = next || total || '0';
+    setDisplayValue(valueToDisplay);
+  };
+
+  return (
+    <div className="calculator">
+      <div className="screen">{displayValue}</div>
+      <div className="buttons">
+        <div className="row">
+          <Button label="AC" color="light-gray" onClick={handleButtonClick} />
+          <Button label="+/-" color="light-gray" onClick={handleButtonClick} />
+          <Button label="%" color="light-gray" onClick={handleButtonClick} />
+          <Button label="÷" color="orange" onClick={handleButtonClick} />
+        </div>
+        <div className="row">
+          <Button label="7" color="gray" onClick={handleButtonClick} />
+          <Button label="8" color="gray" onClick={handleButtonClick} />
+          <Button label="9" color="gray" onClick={handleButtonClick} />
+          <Button label="x" color="orange" onClick={handleButtonClick} />
+        </div>
+        <div className="row">
+          <Button label="4" color="gray" onClick={handleButtonClick} />
+          <Button label="5" color="gray" onClick={handleButtonClick} />
+          <Button label="6" color="gray" onClick={handleButtonClick} />
+          <Button label="-" color="orange" onClick={handleButtonClick} />
+        </div>
+        <div className="row">
+          <Button label="1" color="gray" onClick={handleButtonClick} />
+          <Button label="2" color="gray" onClick={handleButtonClick} />
+          <Button label="3" color="gray" onClick={handleButtonClick} />
+          <Button label="+" color="orange" onClick={handleButtonClick} />
+        </div>
+        <div className="row">
+          <Button label="0" color="gray" doubleSize onClick={handleButtonClick} />
+          <Button label="." color="gray" onClick={handleButtonClick} />
+          <Button label="=" color="orange" onClick={handleButtonClick} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
-const Button = ({ label, color, doubleSize }) => {
+const Button = ({
+  label, color, doubleSize, onClick,
+}) => {
   const buttonStyle = {
     backgroundColor: color === 'orange' ? '#FF8C00' : '#E0E0E0',
   };
 
   return (
-    <button className={`button ${doubleSize ? 'zero' : ''}`} style={buttonStyle} type="button">
+    <button className={`button ${doubleSize ? 'zero' : ''}`} style={buttonStyle} type="button" onClick={() => onClick(label)}>
       {label}
     </button>
   );
@@ -54,6 +76,7 @@ Button.propTypes = {
   label: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
   doubleSize: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
